bb241c31a31c460c6aa53669a6cd74f2ee1f2b2b
Simon Tirant <simon.tirant@gmail.com>
2025-06-11 13:35:17 +0200

    chore: removes unused test files

--- test/frixel_design_system_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule FrixelDesignSystemWeb.PageControllerTest do
-  use FrixelDesignSystemWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
