cd6ee78c873dfb8c1f25e5888c1ec927ff90c25e
Simon Tirant <simon.tirant@gmail.com>
2025-06-11 13:59:47 +0200

    tests: creates test for primary_button component

---
+++ test/frixel_design_system/frixel_components/button_test.exs
@@ -0,0 +1,26 @@
+defmodule FrixelDesignSystem.FrixelComponents.ButtonTest do
+  alias FrixelDesignSystem.FrixelComponents.Button
+  use ComponentCase
+
+  test "primary_button" do
+    assigns = %{}
+
+    html =
+      rendered_to_string(~H"""
+      <Button.primary_button
+        text="Click here"
+        variant="standard"
+        class="p-4"
+        icon_button="hero-arrow-right-solid"
+        type="button"
+      />
+      """)
+
+    assert html =~ "<button"
+    assert html =~ "btn btn-secondary"
+    assert html =~ "Click here"
+    assert html =~ "p-4"
+    assert html =~ "<span class=\"hero-arrow-right-solid"
+    assert html =~ "type=\"button\""
+  end
+end
