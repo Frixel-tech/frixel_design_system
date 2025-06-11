a404b195519689dbf62e6fade226fa4ee6241306
Johanne-Franck <jfranck@frixel.fr>
2025-06-11 12:39:31 +0200

    init repo

---
+++ test/frixel_design_system_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule FrixelDesignSystemWeb.PageControllerTest do
+  use FrixelDesignSystemWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
