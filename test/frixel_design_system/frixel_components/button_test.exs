ba36fcc28267db9fe3a2bc525f070637a92e76a8
Simon Tirant <simon.tirant@gmail.com>
2025-06-11 14:01:31 +0200

    test: adds test for secondary_button

--- test/frixel_design_system/frixel_components/button_test.exs
+++ test/frixel_design_system/frixel_components/button_test.exs
@@ -23,4 +23,25 @@ defmodule FrixelDesignSystem.FrixelComponents.ButtonTest do
     assert html =~ "<span class=\"hero-arrow-right-solid"
     assert html =~ "type=\"button\""
   end
+
+  test "secondary_button" do
+    assigns = %{}
+
+    html =
+      rendered_to_string(~H"""
+      <Button.secondary_button
+        text="Click here"
+        class="p-4"
+        icon_button="hero-arrow-right-solid"
+        type="button"
+      />
+      """)
+
+    assert html =~ "<button"
+    assert html =~ "btn btn-outline"
+    assert html =~ "Click here"
+    assert html =~ "p-4"
+    assert html =~ "<span class=\"hero-arrow-right-solid"
+    assert html =~ "type=\"button\""
+  end
 end
