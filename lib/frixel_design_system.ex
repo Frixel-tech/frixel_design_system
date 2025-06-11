454bdf797d9f7199da9a1bc182d060d3d1ea916a
Simon Tirant <simon.tirant@gmail.com>
2025-06-11 13:33:06 +0200

    feat: creates Button module and import it to lib root module

--- lib/frixel_design_system.ex
+++ lib/frixel_design_system.ex
@@ -6,4 +6,14 @@ defmodule FrixelDesignSystem do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  defmacro __using__(_) do
+    quote do
+      alias FrixelDesignSystem.FrixelComponents.{
+        Button
+      }
+
+      alias FrixelDesignSystem.FrixelSections.{}
+    end
+  end
 end
