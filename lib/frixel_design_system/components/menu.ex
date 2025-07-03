e6d46c02fed53ca456e9728572564ee761b9f8e3
Yannis Mavinga <yannis1.mavinga@epitech.eu>
2025-07-03 17:31:54 +0200

    menu menu-sm dropdown-content bg-base-100 w-screen h-fit

--- lib/frixel_design_system/components/menu.ex
+++ lib/frixel_design_system/components/menu.ex
@@ -171,10 +171,7 @@ defmodule FrixelDesignSystem.Components.Menu do
                   {link.name}
                 </span>
               </div>
-              <ul
-                tabindex="0"
-                class="dropdown-content menu bg-base-100 rounded-box z-50 w-130 p-2 shadow-sm fixed left-0 top-8"
-              >
+              <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 w-screen h-fit">
                 <li :for={sublink <- link.dropdown}>
                   <.link
                     :if={sublink.visibility == :visible}
