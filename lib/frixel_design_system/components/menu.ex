063d3d8adfda1ec5dc3bf73d1b2d80c44736d49c
Yannis Mavinga <yannis1.mavinga@epitech.eu>
2025-07-03 17:42:20 +0200

    bototm

--- lib/frixel_design_system/components/menu.ex
+++ lib/frixel_design_system/components/menu.ex
@@ -161,7 +161,7 @@ defmodule FrixelDesignSystem.Components.Menu do
       <ul class="menu menu-horizontal px-1">
         <li :for={link <- @links}>
           <%= if link[:dropdown] do %>
-            <div class="dropdown dropdown-hover dropdown-end block">
+            <div class="dropdown dropdown-bottom dropdown-end block">
               <div
                 tabindex="0"
                 role="button"
@@ -171,10 +171,7 @@ defmodule FrixelDesignSystem.Components.Menu do
                   {link.name}
                 </span>
               </div>
-              <ul
-                tabindex="0"
-                class="dropdown-content menu bg-base-100 menu menu-sm dropdown-content w-screen/2 h-fit"
-              >
+              <ul tabindex="0" class="dropdown-content menu bg-base-100 menu menu-sm h-fit min-w-max">
                 <li :for={sublink <- link.dropdown}>
                   <.link
                     :if={sublink.visibility == :visible}
