1c2a17606de7727e5ea264b32e97beb64c5f419c
Yannis Mavinga <yannis1.mavinga@epitech.eu>
2025-07-03 15:58:08 +0200

    w-200

--- lib/frixel_design_system/components/menu.ex
+++ lib/frixel_design_system/components/menu.ex
@@ -173,7 +173,7 @@ defmodule FrixelDesignSystem.Components.Menu do
               </div>
               <ul
                 tabindex="0"
-                class="dropdown-content menu bg-base-100 rounded-box z-10 w-100 p-2 shadow-sm"
+                class="dropdown-content menu bg-base-100 rounded-box z-10 w-200 p-2 shadow-sm"
               >
                 <li :for={sublink <- link.dropdown}>
                   <.link
