f03deab2ae1052eb0afec4948e679815c91637bd
Yannis Mavinga <yannis1.mavinga@epitech.eu>
2025-07-04 15:43:04 +0200

    top-20

--- lib/frixel_design_system/components/menu.ex
+++ lib/frixel_design_system/components/menu.ex
@@ -174,7 +174,7 @@ defmodule FrixelDesignSystem.Components.Menu do
               </div>
               <ul
                 tabindex="0"
-                class="dropdown-content bg-base-100 absolute w-screen left-0 top-50 right-0"
+                class="dropdown-content bg-base-100 absolute w-screen left-0 top-20 right-0"
               >
                 <li :for={sublink <- link.dropdown}>
                   <.link
