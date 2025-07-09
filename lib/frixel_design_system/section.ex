64f4a59c54f0e87c04f677cbb5ccef017ee7d369
Yannis Mavinga <yannis1.mavinga@epitech.eu>
2025-07-09 02:14:55 +0200

    vfddf

--- lib/frixel_design_system/section.ex
+++ lib/frixel_design_system/section.ex
@@ -159,7 +159,7 @@ defmodule FrixelDesignSystem.Section do
       <nav class="absolute top-4 right-4 flex items-center gap-4">
         <div class="hidden xl:flex">
           <%= if @is_connected and @user_email do %>
-            <span class="text-sm font-medium px-2 py-1 mr-2">{@user_email}</span>
+            <span class="text-sm font-medium px-2 py-1 mr-4">{@user_email}</span>
           <% end %>
           <Menu.theme_switcher />
         </div>
