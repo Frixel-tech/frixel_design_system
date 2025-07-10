7d815aa5168722d7472b22f845b5e4fcd2e6b84b
Yannis Mavinga <yannis1.mavinga@epitech.eu>
2025-07-10 15:19:22 +0200

    vscd

--- lib/frixel_design_system/section.ex
+++ lib/frixel_design_system/section.ex
@@ -314,28 +314,6 @@ defmodule FrixelDesignSystem.Section do
           </div>
         </div>
       </nav>
-      <nav class="navbar w-3/4 border-t border-gray-300 absolute top-0 left-1/2 transform -translate-x-1/2">
-        <div class="navbar-start flex flex-col lg:flex-row items-center gap-4 no-whitespace mx-auto">
-          <div class="flex flex-col lg:flex-row">
-            <p class="p-4 text-black">
-              Copyright © {@branding_name} {Date.utc_today().year}
-            </p>
-          </div>
-        </div>
-
-        <div class="navbar-end lg:navbar-end flex items-center gap-4 justify-end mx-auto">
-          <div class="flex gap-4">
-            <%= for social <- @social_medias do %>
-              <a
-                href={social.social_media_url}
-                class="block font-common font-normal px-2 py-1 rounded transition hover:underline text-xs"
-              >
-                {social.name}
-              </a>
-            <% end %>
-          </div>
-        </div>
-      </nav>
     </footer>
     """
   end
