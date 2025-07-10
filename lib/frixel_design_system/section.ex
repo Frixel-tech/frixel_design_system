8598c88e7ad1e567fe88faf28256b92c7d992c03
Yannis Mavinga <yannis1.mavinga@epitech.eu>
2025-07-10 15:24:51 +0200

    ,eizei

--- lib/frixel_design_system/section.ex
+++ lib/frixel_design_system/section.ex
@@ -291,11 +291,11 @@ defmodule FrixelDesignSystem.Section do
 
   def base_footer_commerce(assigns) do
     ~H"""
-    <footer class="relative flex flex-col lg:flex-row items-center justify-between py-4 w-full">
+    <footer class="bg-blue relative flex flex-col lg:flex-row items-center justify-between py-4 w-full">
       <nav class="navbar w-3/4 border-t border-gray-300 absolute top-0 left-1/2 transform -translate-x-1/2">
         <div class="navbar-start flex flex-col lg:flex-row items-center gap-4 no-whitespace mx-auto">
           <div class="flex flex-col lg:flex-row">
-            <p class="p-4 text-black">
+            <p class="p-4">
               Copyright © {@branding_name} {Date.utc_today().year}
             </p>
           </div>
