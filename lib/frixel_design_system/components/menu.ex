c4be6a60c6cf0678bc38aa8b5d132a42b5498996
Simon Tirant <simon.tirant@gmail.com>
2025-08-07 13:03:54 +0200

    feat: adds authentication menu to components

--- lib/frixel_design_system/components/menu.ex
+++ lib/frixel_design_system/components/menu.ex
@@ -312,4 +312,54 @@ defmodule FrixelDesignSystem.Components.Menu do
     </label>
     """
   end
+
+  @doc """
+  Authentication menu for USer or Admin connection
+  """
+  attr :current_scope, Scope, doc: "the session current scope as defined by the router plugs"
+  attr :current_path, :string, doc: "the LiveView current path as defined by the router"
+  attr :log_in_path, Phoenix.VerifiedRoutes, doc: "The route path for log in", required: true
+  attr :log_out_path, Phoenix.VerifiedRoutes, doc: "The route path for log out", required: true
+
+  attr :settings_path, Phoenix.VerifiedRoutes,
+    doc: "The route path for user settings",
+    required: false
+
+  attr :registration_path, Phoenix.VerifiedRoutes,
+    doc: "The route path for user registration",
+    required: false
+
+  def auth_menu(assigns) do
+    ~H"""
+    <ul class="menu menu-horizontal w-full relative z-10 flex items-center gap-4 px-4 sm:px-6 lg:px-8 justify-end text-secondary-content font-bold">
+      <%= if @current_scope do %>
+        <li>
+          {@current_scope.user.email}
+        </li>
+        <li :if={@settings_path}>
+          <.link class={@current_path == @settings_path && "menu-active"} href={@settings_path}>
+            {gettext("Settings")}
+          </.link>
+        </li>
+        <li>
+          <.link href={@log_out_path} method="delete">Log out</.link>
+        </li>
+      <% else %>
+        <li :if={@registration_path}>
+          <.link
+            class={@current_path == @registration_path && "menu-active"}
+            href={@registration_path}
+          >
+            {gettext("Register")}
+          </.link>
+        </li>
+        <li>
+          <.link class={@current_path == @log_in_path && "menu-active"} href={@log_in_path}>
+            {gettext("Log in")}
+          </.link>
+        </li>
+      <% end %>
+    </ul>
+    """
+  end
 end
