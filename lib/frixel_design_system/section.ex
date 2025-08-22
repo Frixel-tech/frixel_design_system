57326f97305c8704a973ed7b0b7401214ab97041
Simon Tirant <simon.tirant@gmail.com>
2025-08-22 16:51:52 +0200

    fix: adds the card_class attribute to values_section component

--- lib/frixel_design_system/section.ex
+++ lib/frixel_design_system/section.ex
@@ -364,6 +364,10 @@ defmodule FrixelDesignSystem.Section do
     required: true,
     doc: "A list of values to display in the values cards"
 
+  attr :card_class, :string,
+    default: "",
+    doc: "The CSS classes to put style into your introduction card"
+
   def values_section(assigns) do
     ~H"""
     <section id="values">
@@ -371,7 +375,11 @@ defmodule FrixelDesignSystem.Section do
 
       <div class="flex items-center justify-center flex-wrap gap-14">
         <%= for company_value <- @company_values do %>
-          <Company.company_values_card title={company_value.name} text={company_value.description} />
+          <Company.company_values_card
+            title={company_value.name}
+            text={company_value.description}
+            class={card_class}
+          />
         <% end %>
       </div>
     </section>
