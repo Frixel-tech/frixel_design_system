4a5e55f5d4fae7c9892ac9ba3d10fc1c752de966
Yannis Mavinga <yannis1.mavinga@epitech.eu>
2025-09-08 13:10:12 +0200

    import export new hook

--- assets/js/hooks/index.js
+++ assets/js/hooks/index.js
@@ -1,6 +1,6 @@
-import { CardStackingAnimationHook, CardSlideInHook, DelayedFadeInAnimationHook, FadeInAnimationHook, LateralSlideFromBothSideAnimationHook, ParallaxAnimationHook } from "./gsap-animations-hook";
+import { CardStackingAnimationHook, CardSlideInHook, DelayedFadeInAnimationHook, CatalogFadeInAnimationHook, FadeInAnimationHook, LateralSlideFromBothSideAnimationHook, ParallaxAnimationHook } from "./gsap-animations-hook";
 import GetAndStoreThemeHook from "./get-and-store-theme-hook";
 import LeafletHook from "./leaflet.-hook";
 import ScrollToTopHook from "./scroll-to-top-hook";
 
-export default { CardStackingAnimationHook, CardSlideInHook, DelayedFadeInAnimationHook, FadeInAnimationHook, LateralSlideFromBothSideAnimationHook, ParallaxAnimationHook, GetAndStoreThemeHook, LeafletHook, ScrollToTopHook };
+export default { CardStackingAnimationHook, CardSlideInHook, DelayedFadeInAnimationHook, CatalogFadeInAnimationHook, FadeInAnimationHook, LateralSlideFromBothSideAnimationHook, ParallaxAnimationHook, GetAndStoreThemeHook, LeafletHook, ScrollToTopHook };
