484f0ff42c5cf57f623c326295638e508b919089
Yannis Mavinga <yannis1.mavinga@epitech.eu>
2025-09-23 11:31:35 +0200

    test 13

--- assets/js/hooks/leaflet.-hook.js
+++ assets/js/hooks/leaflet.-hook.js
@@ -9,7 +9,7 @@ const LeafletHook = {
     renderMap() {
         const pointerUrl = this.getIconUrl();
         const coordinates = [this.getLattitude(), this.getLongitude()];
-        let map = L.map(this.el.id).setView(coordinates, 5);
+        let map = L.map(this.el.id).setView(coordinates, 13);
 
         let frixelIcon = L.icon({
             iconUrl: pointerUrl, iconSize: [30, 30]
