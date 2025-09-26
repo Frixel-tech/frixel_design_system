929a564047c7ead98dc6ebe8005b48dc5f152651
Antonin M. <antonin@frixel.fr>
2025-09-26 17:46:52 +0200

    push

--- assets/js/hooks/index.js
+++ assets/js/hooks/index.js
@@ -1,18 +1,18 @@
 import { CardStackingAnimationHook, DelayedFadeInAnimationHook, CatalogFadeInAnimationHook, FadeInAnimationHook, LateralSlideFromBothSideAnimationHook, ParallaxAnimationHook } from "./gsap-animations-hook";
 import GetAndStoreThemeHook from "./get-and-store-theme-hook";
-import LeafletHook from "./leaflet.-hook";
+import LeafletHook from "./leaflet-hook";
 import ScrollToTopHook from "./scroll-to-top-hook";
 import ConditionalPhoneRequiredHook from "./conditional-phone-required-hook";
 
 export default {
-    CardStackingAnimationHook,
-    DelayedFadeInAnimationHook,
-    CatalogFadeInAnimationHook,
-    FadeInAnimationHook,
-    LateralSlideFromBothSideAnimationHook,
-    ParallaxAnimationHook,
-    GetAndStoreThemeHook,
-    LeafletHook,
-    ScrollToTopHook,
-    ConditionalPhoneRequiredHook
+  CardStackingAnimationHook,
+  DelayedFadeInAnimationHook,
+  CatalogFadeInAnimationHook,
+  FadeInAnimationHook,
+  LateralSlideFromBothSideAnimationHook,
+  ParallaxAnimationHook,
+  GetAndStoreThemeHook,
+  LeafletHook,
+  ScrollToTopHook,
+  ConditionalPhoneRequiredHook
 };
