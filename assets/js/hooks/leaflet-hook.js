9100957f02713a2b48ccfd4f0c1635cd34925460
A. MINGAM <43722279+iMaisho@users.noreply.github.com>
2025-09-26 17:50:39 +0200

    feat(find_us_map add marker size and change marker anchor)

--- assets/js/hooks/leaflet-hook.js
+++ assets/js/hooks/leaflet-hook.js
@@ -4,18 +4,24 @@ const LeafletHook = {
     mounted() { this.renderMap() },
     updated() { this.renderMap() },
     getIconUrl() { return this.el.dataset.markerIconUrl },
+    getIconSize() { return this.el.dataset.markerIconSize },
     getLattitude() { return this.el.dataset.lattitude },
     getLongitude() { return this.el.dataset.longitude },
     renderMap() {
         const pointerUrl = this.getIconUrl();
         const coordinates = [this.getLattitude(), this.getLongitude()];
-        let map = L.map(this.el.id).setView(coordinates, 11);
+        let map = L.map(this.el.id).setView(coordinates, 12);
+
+        const iconSize = this.getIconSize();
+        const parsedIconSize = Array.isArray(iconSize) ? iconSize : [iconSize, iconSize];
 
         let frixelIcon = L.icon({
-            iconUrl: pointerUrl, iconSize: [50, 50]
+            iconUrl: pointerUrl,
+            iconSize: parsedIconSize,
+            iconAnchor: [parsedIconSize[0] / 2, parsedIconSize[1] / 2], // Center horizontally, center vertically
         })
 
-        L.marker(coordinates, { icon: frixelIcon }).addTo(map);
+        let marker = L.marker(coordinates, { icon: frixelIcon }).addTo(map);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
