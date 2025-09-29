67c472e84aa1e94f69797ca730fe7d47894c514a
Antonin M. <antonin@frixel.fr>
2025-09-29 16:23:28 +0200

    test

--- assets/js/hooks/leaflet-hook.js
+++ assets/js/hooks/leaflet-hook.js
@@ -15,18 +15,18 @@ const LeafletHook = {
         const pointerUrl = this.getIconUrl();
         const iconCoordinates = [this.getIconLattitude(), this.getIconLongitude()];
         const mapCoordinates = [this.getMapLattitude(), this.getMapLongitude()]
-        let map = L.map(this.el.id).setView(mapCoordinates, 12);
+        let map = L.map(this.el.id).setView(iconCoordinates, 12);
 
         const iconSize = this.getIconSize();
         const parsedIconSize = Array.isArray(iconSize) ? iconSize : [iconSize, iconSize];
 
-        let Icon = L.icon({
+        let markerIcon = L.icon({
             iconUrl: pointerUrl,
             iconSize: parsedIconSize,
             iconAnchor: [parsedIconSize[0] / 2, parsedIconSize[1] / 2], // Center horizontally, center vertically
         })
 
-        let marker = L.marker(iconCoordinates, { icon: frixelIcon }).addTo(map);
+        L.marker(iconCoordinates, { icon: markerIcon }).addTo(map);
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
